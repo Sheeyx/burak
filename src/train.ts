71ea2ebf844376044114f3499aacedd79139a623
Sheeyx <sheeyx00@gmail.com>
2024-02-27 10:07:09 +0900

    BRR: Add task H

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,14 @@
-/*G-TASK: 
+/*H-TASK: 
 
-Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
-MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini. */
+Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+MASALAN: gerReverse("hello") return qiladi "olleh"
 
-function getHighestIndex(array: number[]){
-    let max = 0;
-    array.forEach((element: number) => {
-        if (element > max) {
-            max = element;
-        }
-    });
-    return array.indexOf(max);
+@MITASK
+*/
+
+function gerReverse(str: string){
+    return str.split('').reverse().join('');
 }
 
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-
-console.log("Result: ",result);
+const result = gerReverse("hello");
+console.log(result);
