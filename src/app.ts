7ad72ff9e2539b47e81cdb12d75490087b7dbeba
Sheeyx <sheeyx00@gmail.com>
2024-02-29 02:14:16 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE */
 const app = express();
@@ -17,6 +18,7 @@ app.set('views', path.join(__dirname, "views"));
 app.set('view engine', "ejs");
 
 /** 4-ROUTERS */
-app.use('/', router);
+app.use('/admin', routerAdmin); //EJS
+// app.use('/', router);           //SPA
 
 export default app;  
