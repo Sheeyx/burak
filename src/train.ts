e8996bec278ea4d026f4bc7820bef3e742b54c13
Sheeyx <sheeyx00@gmail.com>
2024-03-01 23:34:33 +0900

    feat: create project standards

--- src/train.ts
+++ src/train.ts
@@ -1,20 +1,11 @@
-// H-TASK: 
+/* Project Standards: 
+ - Logging standards
+ - Naming standards
+     function,method,variable => Camel Case
+     class => Poscal Case
+     folder => KEBAB Case
+     Css => SNAKE Case
 
-// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
-// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+ - Error handling
 
-let nums = "";
-
-function getPositive(array: number[]){
-    array.forEach(element => {
-        if(element>0){
-            nums += element.toString();
-        }
-    });
-
-    return nums;
-}
-
-let result = getPositive([1, -4, 2,5,6,-7]);
-
-console.log(result);
+ */
