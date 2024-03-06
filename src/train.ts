bbc17882107e7dd8dd9e3c2a6f0d9a6070d0b8a7
Sheeyx <sheeyx00@gmail.com>
2024-03-07 02:45:17 +0900

    Add Task I

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,18 @@
-/* Project Standards: 
- - Logging standards
- - Naming standards
-     function,method,variable => Camel Case
-     class => Poscal Case
-     folder, file => KEBAB Case
-     Css => SNAKE Case
+// I-TASK: 
 
- - Error handling
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
 
- */
+function getDigits(str:string){
+    let num: string[] = str.split("");
+    let result = [];
+    for (let i = 0; i < num.length; i++) {
+        if (!isNaN(Number(num[i]))) {
+            result.push(num[i]);
+        } 
+    }
+    return result.join("");
+}
 
- /*
-    Traditional API
-    REST API
-    GraphQL API
-    ....
- */
+let res = getDigits("m14i1t");
+console.log(res);
