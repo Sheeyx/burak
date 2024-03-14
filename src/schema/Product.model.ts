1bb4c78dbf7b3feb844fe802242e307cbbef0d9b
Sheeyx <sheeyx00@gmail.com>
2024-03-14 10:54:57 +0900

    feat: configure product Schema model

---
+++ src/schema/Product.model.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose"; 
+import { ProductCollection, ProductSize, ProductStatus, ProductVolume } from "../libs/enums/products.enum";
+
+const productSchema = new Schema({
+   ProductStatus: {
+    type: String,
+    enum: ProductStatus,
+    default: ProductStatus.PAUSE
+   },
+
+   ProductCollection: {
+    type: String,
+    enum: ProductCollection,
+    required: true
+   },
+
+   ProductName: {
+    type: String,
+    required: true
+   },
+
+   ProductPrice: {
+    type: Number,
+    required: true
+   },
+
+   ProductLeftCount: {
+    type: Number,
+    required: true
+   },
+
+   ProductSize: {
+    type: Number,
+    enum: ProductSize,
+    default: ProductSize.NORMAL
+   },
+
+   ProductVolume: {
+    type: Number,
+    enum: ProductVolume,
+    default: ProductVolume.ONE
+   },
+
+   ProductDesc: {
+    type: String,
+    required: true
+   },
+
+   ProductImages: {
+    type: [String],
+    default: []
+   },
+
+   ProductViews: {
+    type: Number,
+    default: 0
+   },
+},
+{timestamps: true} // updatedAt, createdAt
+);
+productSchema.index(
+    {productName: 1, productSize: 1, productVolume: 1},
+    {unique: true}
+)
+
+export default mongoose.model('Member', productSchema);
