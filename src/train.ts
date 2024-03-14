8f3d283436ba6cfb84213793ecc936fa50e2fbe8
Sheeyx <sheeyx00@gmail.com>
2024-03-14 13:07:12 +0900

    Add Task L

--- src/train.ts
+++ src/train.ts
@@ -1,20 +1,21 @@
-// K-TASK: 
+// L-TASK: 
 
-// Shunday function yozing, u string qabul qilsin va string ichidagi unli harflar sonini qaytarsin.
-// MASALAN: countVowels("string") return 1;
+// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
+// MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
 
 
 
-function countVowels(str: string) {
-  const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-  const strArr: string[] = str.split('');
+
+
+function reverseSentence(str: string) {
+  const strArr: string[] = str.split(' ');
   
-  const findVowel: string[] = strArr.filter((elem: string) =>
-    vowels.includes(elem)
-  );
+  for(let i=0; i<strArr.length; i++){
+    strArr[i] = strArr[i].split('').reverse().join('');
+  }
   
-  return findVowel.length;
+  return strArr;
 }
 
-let result = countVowels("string");
+let result = reverseSentence("we like coding!");
 console.log("Vowels are: ",result);
