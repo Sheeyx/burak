a5d5630b90e966c7c543129e3f03ee9ed5911639
Sheeyx <sheeyx00@gmail.com>
2024-03-18 15:36:49 +0900

    Add TASK-N

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,19 @@
-// M-TASK: 
+// N-TASK: 
 
-// Shunday function yozing, u raqam lard an tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
-// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+// MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
 
 
-function getSquareNumbers(array: number[]) {
-  const myArray: Object[] = [];
-  for (let i = 0; i < array.length; i++) {
-    myArray.push({ key: array[i], value: Math.pow(array[i],2) });
+
+
+function palindromCheck(str: string) {
+  let reverse = str.split("").reverse().join("");
+  if(str === reverse){
+    return true;
+  } else {
+    return false
   }
-  return myArray;
 }
 
-let result = getSquareNumbers([1, 2, 3]);
-console.log("Result ",result);
+let result = palindromCheck("son");
+console.log("Result: ",result);
