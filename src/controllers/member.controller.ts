4e069f9df984a693568f72add92040099e841d1a
Sheeyx <sheeyx00@gmail.com>
2024-03-23 06:06:45 +0900

    fix: modify spa login api process

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -27,22 +27,18 @@ memberController.signup = async (req: Request, res: Response) => {
     }
 }
 
-memberController.login = async(req: Request, res: Response) => {
+memberController.login = async (req: Request, res: Response) => {
     try {
-        console.log("login");
-        const input: LoginInput = req.body;
-        const result = await memberService.login(input);
-
-        //TODO: TOKEN AUTHENTICATION
-
-
-        res.json({member: result});
-      } catch (err) {
-        console.log("Error, on Login Page", err);
-        if(err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard);
-        // res.send(err);
-      }
-}
+      console.log("login");
+      const input: LoginInput = req.body,
+        result = await memberService.login(input);
+      //    TODO: TOKENS AUTHENTICATION
+  
+      res.json({ member: result });
+    } catch (err) {
+      if (err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  };
 
 export default memberController;
