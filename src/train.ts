a2853c586297c5f5e1643464b6279c0c7e8e0eed
Sheeyx <sheeyx00@gmail.com>
2024-03-23 09:34:55 +0900

    Add TASK-P

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,17 @@
-// O-TASK:
+// P-TASK:
 
-// Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
-// MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45
+// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
+// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
 
 
-function calculateSumOfNumbers(arr: any) {
-  let count:number = 0;
-  for(let i = 0; i < arr.length; i++){
-    if(typeof arr[i] === 'number') {      
-      count += arr[i];
-    }
+
+function objectToArray(obj: { [key: string]: any }): [string, string][] {
+  let array: [string, string][] = [];
+  for (let key in obj) {
+    array.push([key, `${obj[key]}`]);
   }
-  return count;
+  return array;
 }
 
-let result = calculateSumOfNumbers([10, "10", {son: 10}, true, 35])
+let result = objectToArray( {a: 10, b: 20})
 console.log("Result: ",result);
