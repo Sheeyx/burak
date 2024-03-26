d4c3083564debd7c588585f08398b67046a3baef
Sheeyx <sheeyx00@gmail.com>
2024-03-26 09:54:18 +0900

    Add TASK-Q

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,17 @@
-// P-TASK:
+// Q-TASK:
 
-// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+// Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+// MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
 
 
-
-function objectToArray(obj: { [key: string]: any }): [string, string][] {
-  let array: [string, string][] = [];
+function hasProperty(obj: { [key: string]: any }, str:string) {
   for (let key in obj) {
-    array.push([key, `${obj[key]}`]);
+    if(key === str){
+      return true;
+    } 
+    return false;
   }
-  return array;
 }
 
-let result = objectToArray( {a: 10, b: 20})
+let result = hasProperty({name: "BMW", model: "M3"}, "name")
 console.log("Result: ",result);
