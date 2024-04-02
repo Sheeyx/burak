845f429ba4579beac978c20fb31a820a09f60646
Sheeyx <sheeyx00@gmail.com>
2024-04-02 09:47:58 +0900

    Add TASK-T

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,16 @@
-// S-TASK:
+// T-TASK:
 
-// Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
-// MASALAN: missingNumber([3, 0, 1]) return 2
+// Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
 
 
-function missingNumber(arr: number[]) {
-    const num = arr.sort()
-  
-  for(let i = num[0]; i<num[num.length-1]; i++){
-    if(num[i] !== i){
-      return i;
-    }
-  }
+
+
+function mergeSortedArrays(arr1: number[],arr2: number[]) {
+    const num = arr1.concat(arr2).sort((a, b) => a - b);
+    return num;
 }
 
-let result = missingNumber([3, 0, 1])
+let result = mergeSortedArrays([0,3,4,31], [4,6,30]);
 console.log("Result: ",result);
