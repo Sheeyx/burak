e73be1187b5852f61568671349f615f78c75f42b
Sheeyx <sheeyx00@gmail.com>
2024-04-09 09:29:33 +0900

    fix: modify getMemberDetail logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -95,6 +95,24 @@ memberController.login = async (
     }
    }
 
+   memberController.getMemberDetail = async(
+    req: ExtendedRequest,
+    res: Response
+   ) => {
+    try {
+      console.log("getMemberDetail");
+
+      const result = await memberService.getMemberDetail(req.member);
+
+      res.status(HttpCode.OK).json(result);
+      
+    } catch (err) {
+      console.log("Error, verifyAuth", err);
+      if(err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+   }
+
   memberController.retrieveAuth = async (
     req: ExtendedRequest, 
     res: Response, 
