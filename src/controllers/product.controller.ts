881952b4eb26756b0dbb169f09b90d2f8db16b2d
Sheeyx <sheeyx00@gmail.com>
2024-04-13 10:11:50 +0900

    feat: develop getProduct rest API

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from '../models/Product.service';
-import { AdminRequest } from '../libs/types/member';
+import { AdminRequest, ExtendedRequest } from '../libs/types/member';
 import { ProductInput, ProductInquire } from '../libs/types/product';
 import { ProductCollection } from '../libs/enums/products.enum';
 
@@ -36,6 +36,24 @@ productController.getProducts = async (req: Request, res: Response)=>{
        
     } catch(err) {
         console.log("Error, getProducts", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+}
+
+productController.getProduct = async (req: ExtendedRequest, res: Response)=>{
+    try {
+        console.log("Error, getProduct");
+
+        const {id} = req.params;
+        const memberId = req.member?._id ?? null,
+            result = await productService.getProduct(memberId, id);
+            
+        res.status(HttpCode.OK).json(result);
+        
+
+    } catch(err) {  
+        console.log("Error, getProduct", err);
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
     }
