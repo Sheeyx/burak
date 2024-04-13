3e8db188b9d75c3e98d82648db8a27dc48248b5d
Sheeyx <sheeyx00@gmail.com>
2024-04-13 15:07:22 +0900

    BRR: Add TASK-Y

--- src/train.ts
+++ src/train.ts
@@ -1,22 +1,15 @@
-// X-TASK:
+// Y-TASK:
 
-// Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
-//  MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
+//  Shunday function yozing, uni 2 ta array parapetri bolsin. Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda qaytarsin
+//  MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
 
 
-function countOccurrences(obj: { [key: string]: any }, key: string): number {
-    let count = 0;
-    for (let value in obj) {
-        if (obj.hasOwnProperty(value)) {
-            if (typeof obj[value] === 'object') {
-                count += countOccurrences(obj[value], key);
-            } else if (value === key) {
-                count++;
-            }
-        }
-    }
-    return count;
+function findIntersection(arr1: number[], arr2: number[]): number[] {
+    const array1 = new Set(arr1);
+    const array2 = new Set(arr2);
+    const intSec = Array.from(array1).filter(x => array2.has(x));
+    return intSec;
 }
 
-let result = countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model');
+let result = findIntersection([1,2,3], [3,2,0]);
 console.log("Result: ",result);
