35fd2df457e3b3e2e32888f7f04382575a5a3d62
Sheeyx <sheeyx00@gmail.com>
2024-04-23 10:54:14 +0900

    BRR: Add TASK-ZC

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,11 @@
-// ZB-TASK:
+// ZC-TASK:
 
-// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
-// MASALAN: randomBetween(30, 50) return 45
+// Shunday function yozing, uni number parametri bolsin va function qabul parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin
+// MASALAN: celsiusToFahrenheit(0) return 32
 
-function randomBetween(min:number, max:number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function celsiusToFahrenheit(celsius:number) {
+    return (celsius * 9 / 5) + 32;
 }
 
-let result = randomBetween(30, 50);
+let result = celsiusToFahrenheit(0);
 console.log("Result: ",result);
