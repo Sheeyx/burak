e69719cd86253cdd6a834616df4179d2743fd338
Sheeyx <sheeyx00@gmail.com>
2024-04-25 14:25:00 +0900

    BRR: Add TASK-ZD

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,19 @@
-// ZC-TASK:
+// ZD-TASK:
 
-// Shunday function yozing, uni number parametri bolsin va function qabul parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin
-// MASALAN: celsiusToFahrenheit(0) return 32
+// Shunday function yozing, uni number, array va number parametrlari bolsin va berilgan 1-parametr numberga teng indexni array ichidan topib 3-parametrdagi raqam bilan almashtirib yangilangan arrayni qaytarsin
+// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2]
 
-function celsiusToFahrenheit(celsius:number) {
-    return (celsius * 9 / 5) + 32;
+
+
+function hangeNumberInArray(i: number, array: number[], newVal: number) {
+    if (i >= 0 && i < array.length) {
+        const newArray = [...array]; 
+        newArray[i] = newVal; 
+        return newArray;
+    } else {
+        return array; 
+    }
 }
 
-let result = celsiusToFahrenheit(0);
+let result = hangeNumberInArray(1, [1,3,7,2], 2) ;
 console.log("Result: ",result);
