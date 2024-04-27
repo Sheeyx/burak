d17063a4e3d69962f559a19a3f0122c519ba0da8
Sheeyx <sheeyx00@gmail.com>
2024-04-27 13:27:12 +0900

    BRR: Add TASK-ZE

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,15 @@
-// ZD-TASK:
+// ZE-TASK:
 
-// Shunday function yozing, uni number, array va number parametrlari bolsin va berilgan 1-parametr numberga teng indexni array ichidan topib 3-parametrdagi raqam bilan almashtirib yangilangan arrayni qaytarsin
-// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2]
+// Shunday function yozing, uni  string parametri bolsin. String ichida takrorlangan harflarni olib tashlab qolganini qaytarsin
+// MASALAN: removeDuplicate('stringg') return 'string'
 
 
-
-function hangeNumberInArray(i: number, array: number[], newVal: number) {
-    if (i >= 0 && i < array.length) {
-        const newArray = [...array]; 
-        newArray[i] = newVal; 
-        return newArray;
-    } else {
-        return array; 
-    }
+function removeDuplicate(str: string) {
+    return str
+        .split('')
+        .filter((char, index, self) => self.indexOf(char) === index)
+        .join('');
 }
 
-let result = hangeNumberInArray(1, [1,3,7,2], 2) ;
+let result = removeDuplicate('stringg');
 console.log("Result: ",result);
