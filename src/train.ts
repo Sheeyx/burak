6132f28a4c427998154ec126e6295c486371e173
Sheeyx <sheeyx00@gmail.com>
2024-05-04 11:05:28 +0900

    BRR: Add TASK-ZH

--- src/train.ts
+++ src/train.ts
@@ -1,12 +1,22 @@
-// ZG-TASK:
+// ZH-TASK:
 
-// Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin. 
-// MASALAN: modifyWords('name should be a string') return 'name_should_be_a_string'
+// Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin. 
+// MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
 
-
-function modifyWords(sentence: string) {
-    return sentence.split(' ').join('_').toLowerCase();
+function findDisappearedNumbers(nums: number[]) {
+    const length = nums.length;
+    const result: number[] = [];
+    
+    for (let i = 0; i < length-1; i++){
+        for(let j = nums[i]; j<nums[i+1]; j++){
+          if(!(nums.includes(j))){
+            result.push(j);
+          }
+        }
+      }
+    
+    return result;
 }
 
-let result = modifyWords('name should be a string');
-console.log("Result: ",result);
+let result = findDisappearedNumbers([1, 3, 4, 7]);
+console.log("Result: ", result);
