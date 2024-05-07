747296841465ed6382847b75dddec1aeddf2b25f
Sheeyx <sheeyx00@gmail.com>
2024-05-07 13:15:45 +0900

    BRR: Add TASK - ZI

--- src/train.ts
+++ src/train.ts
@@ -1,22 +1,19 @@
-// ZH-TASK:
+// ZI-TASK:
 
-// Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin. 
-// MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
+// Shunday function yozing, u function ishga tushgandan 3 soniyadan keyin "Hello World" ni qaytarsin.
+// MASALAN: delayHelloWorld("Hello World") return "Hello World"
 
-function findDisappearedNumbers(nums: number[]) {
-    const length = nums.length;
-    const result: number[] = [];
-    
-    for (let i = 0; i < length-1; i++){
-        for(let j = nums[i]; j<nums[i+1]; j++){
-          if(!(nums.includes(j))){
-            result.push(j);
-          }
-        }
-      }
-    
-    return result;
+async function delayHelloWorld(message: string) {
+  return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+          resolve(message);
+      }, 3000);
+  });
 }
 
-let result = findDisappearedNumbers([1, 3, 4, 7]);
-console.log("Result: ", result);
+async function main() {
+  let result = await delayHelloWorld("Hello World");
+  console.log("Result: ", result);
+}
+
+main();
